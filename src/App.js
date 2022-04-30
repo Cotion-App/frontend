5159911c0a0778c365ed83396e76ab4bc94b571c
abhiram <abhiramg2021@gmail.com>
2022-04-30 19:49:51 -0400

    changed application to work off my personal information rather than a public integration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import notion_dark from "./media/notion-dark.png";
-import notion from "./media/notion.png"
 import { FiGithub } from "react-icons/fi";
 import { useState } from "react";
 import toast, { Toaster } from 'react-hot-toast';
@@ -11,7 +9,6 @@ import { getThings } from "./utils/utils.js";
 
 function App() {
   let counter = 0;
-  const [hovered, setHovered] = useState(false);
   const [canvasToken, setCanvasToken] = useState("")
   const [courseURL, setCourseURL] = useState("")
 
@@ -63,13 +60,6 @@ function App() {
       <div className="content font-serif md:w-4/6 mx-auto p-5 max-w-xl select-none">
         <div className="header text-3xl pb-2 font-bold"> Cotion</div>
         <div className="body space-y-5 border-y-2 border-black py-4">
-          <div className="notion-auth border-2 border-red-500 rounded-md p-4">
-            <div className="mb-2">Please click the button below to reauthorize with Notion.</div>
-            <button className="flex items-center" onMouseOver={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
-              <img src={hovered ? notion : notion_dark} alt="N" className="object-scale-down h-8 mr-2" />
-              Add to Notion
-            </button>
-          </div>
           <div className="paste-link flex">
             {stepNumbering()} Paste the link to your course here: <input onChange={e => setCourseURL(e.target.value)} />
           </div>
@@ -81,11 +71,6 @@ function App() {
           <div className="canvas-access-token">
             {stepNumbering()} Go to Canvas settings, scroll down, and click
             '+ New Access Token'. Paste your Access Token here:<input onChange={e => setCanvasToken(e.target.value)} />
-            <div className="local-storage text-sm ml-5 flex items-center">
-              <input class="form-check-input rounded-sm mr-2 checked:hover:bg-black checked:bg-black"
-                type="checkbox" />
-              Do you want to save this information to local storage?
-            </div>
           </div>
 
           <div className="duplicate-notion-table">
@@ -95,9 +80,7 @@ function App() {
           </div>
 
           <div className="share-table">
-            {stepNumbering()} Share the duplicated table with the 'Cotion' integration by clicking
-            'Add people, emails, groups, or integrations' box and typing 'Cotion' in
-            the search box.
+            {stepNumbering()} Share the duplicated table with Abhiram Ghanta on Notion.
           </div>
           <div className="share-token">
             {stepNumbering()} While you're in the 'Share' panel,
