873f6cec8df11598def5145c0efd86c8897a4e42
abhiram <abhiramg2021@gmail.com>
2022-04-30 22:29:49 -0400

    updated api call

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,7 +4,7 @@ import { BACKEND_URL } from "./constants.js";
 export async function getThings(domain, courseID, dbID, course) {
   return (
     axios
-      .get(`${BACKEND_URL}/run/${domain}/${courseID}/${dbID}/${course}`)
+      .get(`${BACKEND_URL}/run/${domain}/${courseID}/${course}/${dbID}`)
       .then((response) => console.log(response.data))
   );
 }
